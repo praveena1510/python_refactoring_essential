dd05427c32317c2fabb400e359bb8e5e2e7c906b
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-09 10:34:21 +0100

    Renamed method parameter q to lower_bound

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -1,11 +1,11 @@
 class X1:
 
     @staticmethod
-    def m(q, z):
+    def m(lower_bound, z):
         p = 0
 
         # Iterate from lower bound (q) to upper bound (z)
-        for i in range(q, z + 1):
+        for i in range(lower_bound, z + 1):
             # Add square of each number in the range
             p += X1.n(i)
 
