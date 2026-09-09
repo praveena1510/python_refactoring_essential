215bb3bdb4ab3af4f315ae2cb190f4591ce04c10
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-09 10:52:41 +0100

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -1,11 +1,11 @@
-class X1:
+class RangeCalculator:
 
     @staticmethod
     def sum_of_squares_in_range(lower_bound, upper_bound):
         accumulated_sum = 0
 
         for i in range(lower_bound, upper_bound + 1):
-            accumulated_sum += X1.square(i)
+            accumulated_sum += RangeCalculator.square(i)
 
         return accumulated_sum
 
