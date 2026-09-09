a85e236310edec601f7b30b593c2d69233a975ad
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-09 10:48:26 +0100

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -7,12 +7,12 @@ class X1:
         # Iterate from lower bound (q) to upper bound (z)
         for i in range(lower_bound, upper_bound + 1):
             # Add square of each number in the range
-            accumulated_sum += X1.n(i)
+            accumulated_sum += X1.square(i)
 
         # Return accumulated sum
         return accumulated_sum
 
     @staticmethod
-    def n(k):
+    def square(k):
         # Return square of input
         return k * k
