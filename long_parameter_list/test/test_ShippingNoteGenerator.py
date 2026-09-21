f1f47d4e162856dc4e71fd597857aceaa654de26
Vootkuri, Swetha (Automation Developer - Sky) <swetha.vootkuri+comcast@sky.uk>
2026-09-21 10:40:41 +0100

    It works!

--- long_parameter_list/test/test_ShippingNoteGenerator.py
+++ long_parameter_list/test/test_ShippingNoteGenerator.py
@@ -42,7 +42,7 @@ class ShippingNoteGeneratorTest(unittest.TestCase):
 
     def test_should_include_full_address_across_all_fields(self):
         result = self.shipping_note_generator.generate_shipping_note("Emma", "Jones", "99 High Road", "Floor 2",
-                                                                     "Leeds", "LS1 4AB", "UK", "ORD-777", "Monitor", 3)
+                                                                     "Leeds", "LS1 4AB", "UK", "ORD-777", "Monitor", 3, Customer("Emma","Jones"))
 
         self.assertIn("99 High Road", result)
         self.assertIn("Floor 2", result)
