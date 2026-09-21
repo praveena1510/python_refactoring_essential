9482cba2b1e3b058163e26593871ea59f6037a91
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-21 10:50:23 +0100

    It works!

--- long_parameter_list/src/ShippingNoteGenerator.py
+++ long_parameter_list/src/ShippingNoteGenerator.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 class ShippingNoteGenerator:
 
     def generate_shipping_note(self, address_line1, address_line2, city, postcode, country, order_id, item_description,
-                               quantity, customer=None):
+                               quantity, customer=None, address=None):
         full_name = customer.first_name + " " + customer.last_name
 
         address = address_line1 + ", " + (
