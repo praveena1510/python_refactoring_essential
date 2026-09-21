7672aaf609defee9794de9e74b752b615f51276b
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-09-21 10:25:45 +0100

    It works!

--- duplication/src/WeatherReport.py
+++ duplication/src/WeatherReport.py
@@ -32,7 +32,7 @@ class WeatherReport:
         for forecast in forecasts:
 
             if forecast.is_morning():
-                time_of_the_day = forecast.period[0:1].capitalize() + forecast.period[1::]
+                time_of_the_day = forecast.period.capitalize()
                 line = (
                     time_of_the_day + ": "
                     + str(forecast.get_temperature())
